1a6a84b4eba523c376d42abcc3cff82d146d57ec
Marcos Sanchez <msanchez@hircasa.com>
2022-08-16 15:46:51 -0500

    Opciones de pokemons

---
+++ pokemon-game/src/main.js
@@ -0,0 +1,6 @@
+import "./css/styles.css";
+import "./css/animations.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
---
+++ pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>¿Quién es este pokemon?</h1>
+  <PokemonPicture :pokemonId="100" :showPokemon="true" />
+  <PokemonOptions :pokemons="pokemonsArr" />
+</template>
+
+<script>
+import PokemonOptions from "../components/PokemonOptions.vue";
+import PokemonPicture from "../components/PokemonPicture.vue";
+import getPokemonOptions from "../helpers/getPokemonOptions";
+
+export default {
+  name: "PokemonPage",
+  components: { PokemonPicture, PokemonOptions },
+  data() {
+    return {
+      pokemonsArr: [],
+    };
+  },
+  methods: {
+    async mixedPokemonArray() {
+      this.pokemonsArr = await getPokemonOptions();
+      console.log(this.pokemonsArr);
+    },
+  },
+  mounted() {
+    this.mixedPokemonArray();
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
