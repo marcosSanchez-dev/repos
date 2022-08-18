ee9c77299ab0b1db7ff2ca7f0d63885c260d0ddc
Marcos Sanchez <msanchez@hircasa.com>
2022-08-17 19:35:47 -0500

    peticiones Http en router

--- rutas-ciclo/src/modules/pokemon/pages/PokemonPage.vue
+++ rutas-ciclo/src/modules/pokemon/pages/PokemonPage.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>
-    Pokemon #<span>{{ pokemonId }}</span>
+    Pokemon {{ pokemonName }} #<span>{{ pokemonId }}</span>
   </h1>
+  <img :src="imgSrc" :alt="pokemonName" />
 </template>
 
 <script>
@@ -12,9 +13,39 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      imgSrc: null,
+      pokemon: null,
+      pokemonName: null,
+    };
+  },
   created() {
-    // console.log(this.$route);
+    console.log(this.$route);
     // console.log(this.$route.params.pokemonId);
+    this.getPokemon();
+  },
+  methods: {
+    async getPokemon() {
+      try {
+        const pokemon = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${this.pokemonId}`
+        ).then((r) => r.json());
+
+        this.pokemon = pokemon;
+        this.pokemonName = pokemon.name;
+
+        this.imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
+      } catch (error) {
+        this.$router.push("./");
+        console.log("estas en el home");
+      }
+    },
+  },
+  watch: {
+    pokemonId() {
+      this.getPokemon();
+    },
   },
 };
 </script>
